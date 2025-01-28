f9e53c9ad1bd044bccbd12506cabc0264a2fb4a0
Vito Chianese <vito.chianese@outlook.it>
2025-01-28 15:09:07 +0100

    Secondo commit: creazione Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+function Navbar() {
+    return(
+        <ul>
+          <li>Home</li>
+          <li>Chi siamo</li>
+          <li>Consulenza</li>
+          <li>Contatti</li>
+        </ul>
+    )
+}
+
+export default Navbar;
